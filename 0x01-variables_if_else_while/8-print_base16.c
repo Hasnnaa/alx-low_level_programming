a8b835545e61728738b0dad603cf9ecbb8ac0a93
hasnnaa <hasnnaah74@gmail.com>
2023-10-06 15:00:22 -0700

    task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int i = 48; i <= 102; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
