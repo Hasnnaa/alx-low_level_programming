577cb40a258197cfc5af3d373052d00e3a274c6d
hasnnaa <hasnnaah74@gmail.com>
2023-10-06 11:55:46 -0700

    task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char n = 'a'; n <= 'z'; n++)
+{
+putchar (n);
+}
+for (char i = 'A'; i <= 'Z'; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
