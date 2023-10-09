cbdd616bfca15c85e1c929687eb993b43c105320
hasnnaa <hasnnaah74@gmail.com>
2023-10-09 04:58:54 -0700

    task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,13 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    int n = 0;
-    while(n  < 10){
-        putchar('0'+ n);
-        n++;
-    }
-    putchar('\n');
-    return 0;
+int n = 0;
+while (n < 10)
+{
+putchar('0' + n);
+n++;
+}
+putchar('\n');
+return (0);
 }
