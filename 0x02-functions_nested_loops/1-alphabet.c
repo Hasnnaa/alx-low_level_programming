b056db1e0056e60b662d0afb93091bf65642422e
hasnnaa <hasnnaah74@gmail.com>
2023-10-10 06:59:06 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n = 'a';
+while (n <= 'z')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
 #include "main.h"
 
 void printf(char c);
-
+void print_alphabet();
