b97f2b87764397addf5b682c413c5db556c5da71
hasnnaa <hasnnaah74@gmail.com>
2023-10-11 04:35:30 -0700

    task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,14 @@
-include "main.h"
+#include "main.h"
 
 /**
- * postitive_or_negative() - check the code
- *
- * Return: Always 0.
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void postitive_or_negative(void)
+int main(void)
 {
 int i;
-srand(time(0));
-i = rand() - RAND_MAX / 2;
-if (i == 0)
-{
-printf("%d is zero\n", i);
+i = 98;
+positive_or_negative(i);
+return (0);
 }
