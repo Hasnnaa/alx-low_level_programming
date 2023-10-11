76d224741cf8e7798f6aca0b8b75e90437da954c
hasnnaa <hasnnaah74@gmail.com>
2023-10-11 04:33:34 -0700

    task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,16 +11,7 @@ void postitive_or_negative(void)
 int i;
 srand(time(0));
 i = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
+if (i == 0)
 {
 printf("%d is zero\n", i);
-}
-else if (i < 0)
-{
-printf("%d is negative\n", i);
 }
