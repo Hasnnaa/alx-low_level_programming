dddadbdc5d02c7f4502bcfea74be81787844afeb
hasnnaa <hasnnaah74@gmail.com>
2023-11-04 09:32:29 -0700

    t

--- 0-whatsmyname.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char **argv)
-{
-char *mynameis = argv[0];
-printf("%s\n", mynameis);
-return (0);
-}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_numbers(void)
+{
+char c = '0';
+while ( c<= '9')
+{
+putchar(c);
+}
+putchar('\n');
+}
