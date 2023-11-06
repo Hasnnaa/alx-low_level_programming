09a982bf870ffcd5692f387a750301e51c3f9a12
hasnnaa <hasnnaah74@gmail.com>
2023-11-06 15:11:00 -0800

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
